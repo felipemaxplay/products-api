ec458013777b041af06600d06a2423749d1054a3
Felipe Gomes <felipemaxplay@pop-os.localdomain>
2021-09-03 17:34:57 -0300

    add dependencia starter-cache e starter-redis

--- src/main/java/br/com/felipemaxplay/products/ProductsApplication.java
+++ src/main/java/br/com/felipemaxplay/products/ProductsApplication.java
@@ -2,8 +2,10 @@ package br.com.felipemaxplay.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ProductsApplication {
 
 	public static void main(String[] args) {
